b0137b38afcabb8b2c14ad659aec5dbb466c3956
egorgerard <82804542+egorgerard@users.noreply.github.com>
2024-03-12 18:47:39 +0100

    Added GameSession and Player Class

---
+++ WPFUI/Engine/Models/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models{
+    public class Player{
+        public string Name { get; set; }
+        public string Class_ { get; set; }
+        public int Hit_points { get; set; }
+        public int Exp { get; set; }
+        public int Lvl { get; set; }
+        public int Gold { get; set; }
+
+    }
+}
